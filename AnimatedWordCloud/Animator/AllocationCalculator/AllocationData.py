0ffad0bcd91044171225f0b75a0c5ff82003ad0f
konbraphat51 <konbraphat@gmail.com>
2023-11-16 23:38:16 +0900

    Add: word add method

--- AnimatedWordCloud/Animator/AllocationCalculator/AllocationData.py
+++ AnimatedWordCloud/Animator/AllocationCalculator/AllocationData.py
@@ -23,6 +23,20 @@ class AllocationInFrame:
         # word -> (font size, left-top position)
         self.words: Dict[str, Tuple[float, Tuple[float, float]]] = {}
 
+    def add(
+        self, word: str, font_size: float, left_top: Tuple[float, float]
+    ) -> None:
+        """
+        Add a word
+
+        :param str word: Word to add
+        :param float font_size: Font size of the word
+        :param Tuple[float, float] left_top: Left-top position of the word
+        :rtype: None
+        """
+
+        self.words[word] = (font_size, left_top)
+
 
 class AllocationTimelapse:
     """
