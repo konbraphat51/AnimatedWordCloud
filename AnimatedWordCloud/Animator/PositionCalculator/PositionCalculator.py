c7aa1275b429e1ff68a7b0023686b1dfbd325d73
konbraphat51 <konbraphat@gmail.com>
2023-11-16 16:54:08 +0900

    Add: Integration module of position calculation

---
+++ AnimatedWordCloud/Animator/PositionCalculator/PositionCalculator.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2023 AnimatedWordCloud Project
+# https://github.com/konbraphat51/AnimatedWordCloud
+#
+# Licensed under the MIT License.
+"""
+Calculate positions and size of words during each frame
+"""
+
+from typing import List
+from AnimatedWordCloud import TimelapseWordVector
+
+
+class PositionInFrame:
+    """
+    Positions and size of each words
+    """
+
+    pass
+
+
+def calculate(word_vector_timelapse: TimelapseWordVector) -> List[PositionInFrame]:
+    """
+    Calculate positions and size of words during each frame
+
+    :param TimelapseWordVector word_vector_timelapse:
+    :return: Position/size data of each video frame
+    :rtype: List[PositionInFrame]
+    """
+
+    return word_vector_timelapse
