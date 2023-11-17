a5f4f3a3f846e5f630053f0857ad1a2cc0bc6276
konbraphat51 <konbraphat@gmail.com>
2023-11-17 21:11:06 +0900

    Add: converter module from test_data

---
+++ tests/TestDataGetter.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2023 AnimatedWordCloud Project
+# https://github.com/konbraphat51/AnimatedWordCloud
+#
+# Licensed under the MIT License.
+"""
+Get data from test_data submodule
+"""
+
+import json
+from typing import Dict, List, Tuple
+from AnimatedWordCloud.Animator.TimelapseWordVector import TimelapseWordVector
+
+
+def get_test_timelapses_raw() -> List[List[Tuple[str, Dict[str, float]]]]:
+    """
+    Get the raw data of word vectors from test_data submodule
+
+    :return: The list of raw data of word vector timelapses
+    :rtype: List[Tuple[str, Dict[str, float]]]
+    """
+
+    output = []
+
+    # from Elon Musk's tweets
+    with open(
+        "tests/test_data/ElonMusk/wordvector_timelapse_elon.json", "r"
+    ) as f:
+        output.append(json.load(f))
+
+    return output
+
+
+raw_timelapses_test = get_test_timelapses_raw()
+"""
+The raw data of word vector timelapses got from test_data submodule
+:rtype: List[List[Tuple[str, Dict[str, float]]]]
+"""
+
+timelapses_test = []
+"""
+The inner class data of word vector timelapses got from test_data submodule
+:rtype: List[TimelapseWordVector]
+"""
+for raw_timelapse in raw_timelapses_test:
+    timelapse = TimelapseWordVector.convert_from_dicts_list(raw_timelapse)
+    timelapses_test.append(timelapse)
