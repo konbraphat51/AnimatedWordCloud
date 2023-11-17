f9ec99cc0e702df88aca49fd6f2513cae12bd1ec
konbraphat51 <konbraphat@gmail.com>
2023-11-17 21:14:23 +0900

    Add: classes to init
    for easier access
    
    black used

--- AnimatedWordCloud/Animator/__init__.py
+++ AnimatedWordCloud/Animator/__init__.py
@@ -2,6 +2,17 @@ from AnimatedWordCloud.Animator.AllocationCalculator.AllocationData import (
     AllocationInFrame,
     AllocationTimelapse,
 )
+from AnimatedWordCloud.Animator.TimelapseWordVector import (
+    TimelapseWordVector,
+    TimeFrame,
+    WordVector,
+)
 
 
-__all__ = ["AllocationInFrame", "AllocationTimelapse"]
+__all__ = [
+    "AllocationInFrame",
+    "AllocationTimelapse",
+    "TimelapseWordVector",
+    "TimeFrame",
+    "WordVector",
+]
