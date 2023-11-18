b0e26be04003518113ed1e527e787cdf9d54de63
konbraphat51 <konbraphat@gmail.com>
2023-11-18 14:24:51 +0900

    Revert: match to dev
    Solves #25

---
+++ AnimatedWordCloud/Animator/AllocationCalculator/AllocationCalculator.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2023 AnimatedWordCloud Project
+# https://github.com/konbraphat51/AnimatedWordCloud
+#
+# Licensed under the MIT License.
+"""
+Calculate positions and size of words during each frame
+"""
+
+from typing import List
+from AnimatedWordCloud import TimelapseWordVector
+from AnimatedWordCloud.Animator import AllocationInFrame
+
+
+def calculate(
+    word_vector_timelapse: TimelapseWordVector,
+) -> List[AllocationInFrame]:
+    """
+    Calculate positions and size of words during each frame
+
+    :param TimelapseWordVector word_vector_timelapse:
+    :return: Position/size data of each video frame
+    :rtype: List[PositionInFrame]
+    """
+
+    return word_vector_timelapse
