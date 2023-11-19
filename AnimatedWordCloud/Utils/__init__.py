2a967720845d3aea8180569807b5949b3d7abe91
konbraphat51 <konbraphat@gmail.com>
2023-11-20 05:52:22 +0900

    Add: new const to init

--- AnimatedWordCloud/Utils/__init__.py
+++ AnimatedWordCloud/Utils/__init__.py
@@ -1,3 +1,7 @@
-from AnimatedWordCloud.Utils.Consts import LIBRARY_DIR, DEFAULT_ENG_FONT_PATH
+from AnimatedWordCloud.Utils.Consts import (
+    LIBRARY_DIR,
+    DEFAULT_ENG_FONT_PATH,
+    TRANSITION_SYMBOL,
+)
 
-__all__ = ["LIBRARY_DIR", "DEFAULT_ENG_FONT_PATH"]
+__all__ = ["LIBRARY_DIR", "DEFAULT_ENG_FONT_PATH", "TRANSITION_SYMBOL"]
