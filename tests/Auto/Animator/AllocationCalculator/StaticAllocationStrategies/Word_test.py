c8b4378897deca8742cc55d57a77043c4d7e6622
konbraphat51 <konbraphat@gmail.com>
2023-11-20 07:02:04 +0900

    Add: testing word

---
+++ tests/Auto/Animator/AllocationCalculator/StaticAllocationStrategies/Word_test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2023 AnimatedWordCloud Project
+# https://github.com/konbraphat51/AnimatedWordCloud
+#
+# Licensed under the MIT License.
+"""
+Testing the Word module
+"""
+
+from AnimatedWordCloud.Animator.AllocationCalculator.StaticAllocationStrategies.Word import (
+    Word,
+)
+
+
+def test_word():
+    instance = Word("test", 10, 20, (0, 0))
+    assert instance.text == "test"
+    assert instance.weight == 10
+    assert instance.font_size == 20
+    assert instance.text_size == (0, 0)
