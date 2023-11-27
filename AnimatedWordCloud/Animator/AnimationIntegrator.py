0622a92c494465b0707fe8c7bd86a4410deff965
SuperHotDogCat <siromisochan@gmail.com>
2023-11-27 20:16:26 +0900

    Define AnimationIntegrator.integrate_images

--- AnimatedWordCloud/Animator/AnimationIntegrator.py
+++ AnimatedWordCloud/Animator/AnimationIntegrator.py
@@ -6,3 +6,37 @@
 """
 Integrates the images into a single video (gif)
 """
+
+from typing import List
+import cv2
+
+
+def integrate_images(
+    image_paths: List[str],
+    filename: str,
+) -> None:
+    """
+    Create images of each frame
+
+    :param
+    List[str] image_paths: List of image_paths created by AnimatedWordCloud.Animator.ImageCreator.create_images.
+    str filename: output filename.
+    :return: None
+    """
+
+    images = [cv2.imread(path) for path in image_paths]
+    height, width, layers = images[0].shape
+
+    gif = cv2.VideoWriter(
+        filename,
+        cv2.VideoWriter_fourcc(*"GIF"),
+        1,
+        (width, height),
+    )
+
+    for image in images:
+        gif.write(image)
+    cv2.destroyAllWindows()
+    gif.release()
+
+    return
