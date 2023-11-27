0bc818a1f1686fec899a08ae22224146e92713ec
SuperHotDogCat <siromisochan@gmail.com>
2023-11-27 19:46:52 +0900

    initial commit

---
+++ AnimatedWordCloud/Animator/AnimationIntegrator.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2023 AnimatedWordCloud Project
+# https://github.com/konbraphat51/AnimatedWordCloud
+#
+# Licensed under the MIT License.
+"""
+Integrates the images into a single video (gif)
+"""
