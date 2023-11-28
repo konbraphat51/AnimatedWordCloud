d32a56b86013e3ed37db49d49395dbe37d278ad6
SuperHotDogCat <siromisochan@gmail.com>
2023-11-29 00:22:32 +0900

    refac black

--- tests/Auto/Animator/AnimationIntegrator_test.py
+++ tests/Auto/Animator/AnimationIntegrator_test.py
@@ -2,8 +2,9 @@ from AnimatedWordCloud.Animator.AnimationIntegrator import integrate_images
 from typing import List
 from PIL import Image
 
+
 def test_integrateimages():
-    image_paths: List[str] = ["SnapShot.png", "SnapShot.jpg"] #temporary path
+    image_paths: List[str] = ["SnapShot.png", "SnapShot.jpg"]  # temporary path
     filename: str = "output.gif"
     duration: int = 500
     assert integrate_images(image_paths, filename, duration) == None
