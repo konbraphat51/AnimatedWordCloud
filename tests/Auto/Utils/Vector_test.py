470afd73b52314a2f994635b558962514100d77f
konbraphat51 <konbraphat@gmail.com>
2023-11-29 12:03:20 +0900

    Refac: use black

--- tests/Auto/Utils/Vector_test.py
+++ tests/Auto/Utils/Vector_test.py
@@ -9,28 +9,29 @@ Testing the Vector classes
 
 from AnimatedWordCloud.Utils import Vector
 
+
 def test_vector():
     vec1 = Vector(1, 2)
     vec2 = Vector((3, 4))
-    
-    #plus
+
+    # plus
     plus = vec1 + vec2
     assert plus.x == 4
     assert plus.y == 6
-    
-    #minus
+
+    # minus
     minus = vec1 - vec2
     assert minus.x == -2
     assert minus.y == -2
-    
-    #clone
+
+    # clone
     clone = vec1.clone()
     clone += vec2
     assert clone.x == 4
     assert clone.y == 6
     assert vec1.x == 1
     assert vec1.y == 2
-    
-    #convert to tuple
+
+    # convert to tuple
     tup = vec1.convert_to_tuple()
     assert tup == (1, 2)
