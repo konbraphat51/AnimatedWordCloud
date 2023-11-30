276c171233e52f3eadc33929fdcab0cfa2b9d4af
SuperHotDogCat <siromisochan@gmail.com>
2023-11-30 17:42:55 +0900

    fix testcode

--- tests/Auto/Animator/AnimationIntegrator_test.py
+++ tests/Auto/Animator/AnimationIntegrator_test.py
@@ -1,10 +1,14 @@
 from AnimatedWordCloud.Animator.AnimationIntegrator import integrate_images
 from typing import List
-from PIL import Image
+from pathlib import Path
+import os
 
-
+DIR = Path(__file__).parent
 def test_integrateimages():
-    image_paths: List[str] = ["tests0.png", "tests1.png"]  # temporary path
+    image_path0 = os.path.join(DIR, "tests0.png")
+    image_path1 = os.path.join(DIR, "tests1.png")
+    image_paths: List[str] = [image_path0, image_path1]  # temporary path
+    print(image_path1)
     filename: str = "output.gif"
     duration: int = 500
     assert integrate_images(image_paths, filename, duration) == None
