8c086bea2b367652b5f92825b160f84f0bf3796d
konbraphat51 <konbraphat@gmail.com>
2023-12-26 10:57:09 +0900

    Fix: use less data

--- tests/Manual/StaticAllocationVisualization.py
+++ tests/Manual/StaticAllocationVisualization.py
@@ -17,6 +17,6 @@ timelapse_less.timeframes = timelapse.timeframes[:2]
 
 config = Config(max_words=50, max_font_size=20, min_font_size=10, verbosity="debug")
 
-allocation_timelapse = allocate_all(timelapse, config)
+allocation_timelapse = allocate_all(timelapse_less, config)
 
 create_images(allocation_timelapse, config)
