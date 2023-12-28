62befdb26a29bc01bd67a4b4ed50e3869f8218df
SuperHotDogCat <siromisochan@gmail.com>
2023-12-28 19:01:52 +0900

    fix create_images' arguments

--- tests/Auto/Animator/AnimationIntegrator_test.py
+++ tests/Auto/Animator/AnimationIntegrator_test.py
@@ -16,26 +16,22 @@ DIR = Path(__file__).parent
 
 
 def test_integrateimages():
+    config = Config(output_path= "output.gif")
     image_path0 = os.path.join(DIR, "tests0.png")
     image_path1 = os.path.join(DIR, "tests1.png")
     image_paths: list[str] = [image_path0, image_path1]  # temporary path
-    filename: str = "output.gif"
-    duration_per_frame: int = 500
-    assert integrate_images(image_paths, filename, duration_per_frame) == None
+    assert integrate_images(image_paths, config) == None
 
 
 def test_imagecreator_and_integrateimages():
     # test create_images function
+    config = Config(output_path= "output.gif")
     position_in_frames = AllocationTimelapse()
     allocation_in_frame = AllocationInFrame()
     allocation_in_frame.words = {"word": (30, (50, 50))}  # dictionary
     position_in_frames.add("2023_04_01", allocation_in_frame)
     image_paths = create_images(
         position_in_frames,
-        image_size=(100, 100),
-        font_path=DEFAULT_ENG_FONT_PATH,
+        config,
     )
-    filename: str = "output.gif"
-    duration_per_frame: int = 500
-    config = Config()
     assert integrate_images(image_paths, config) == None
