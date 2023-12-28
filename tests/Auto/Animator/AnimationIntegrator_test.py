8b442a56c5e9599b45e7d1d1f64ba79b9adf4d7d
SuperHotDogCat <siromisochan@gmail.com>
2023-12-28 18:50:01 +0900

    fix animation integrator test code

--- tests/Auto/Animator/AnimationIntegrator_test.py
+++ tests/Auto/Animator/AnimationIntegrator_test.py
@@ -11,8 +11,11 @@ from AnimatedWordCloud.Utils.Consts import (
     DEFAULT_ENG_FONT_PATH,
 )
 from AnimatedWordCloud.Animator.AnimationIntegrator import integrate_images
+from AnimatedWordCloud.Utils import Config
 
 DIR = Path(__file__).parent
+
+
 def test_integrateimages():
     image_path0 = os.path.join(DIR, "tests0.png")
     image_path1 = os.path.join(DIR, "tests1.png")
@@ -20,6 +23,7 @@ def test_integrateimages():
     filename: str = "output.gif"
     duration_per_frame: int = 500
     assert integrate_images(image_paths, filename, duration_per_frame) == None
+
 
 def test_imagecreator_and_integrateimages():
     # test create_images function
@@ -34,4 +38,5 @@ def test_imagecreator_and_integrateimages():
     )
     filename: str = "output.gif"
     duration_per_frame: int = 500
-    assert integrate_images(image_paths, filename, duration_per_frame) == None
+    config = Config()
+    assert integrate_images(image_paths, config) == None
