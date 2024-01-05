b2ec3bed32ad4a1f68adcec72a1c1e0113ee62b3
konbraphat51 <konbraphat@gmail.com>
2024-01-05 16:36:58 +0900

    Add: total tester
    test animate()

---
+++ tests/Manual/AnimationVisualization_test.py
@@ -0,0 +1,15 @@
+"""
+Observes how animation words
+"""
+
+from AnimatedWordCloud import Config, animate
+from tests.TestDataGetter import timelapses_test
+
+#testing data
+timelapse = timelapses_test[0]
+
+config = Config(
+    max_words=50, max_font_size=20, min_font_size=10, verbosity="debug"
+)
+
+animate(timelapse, config)
