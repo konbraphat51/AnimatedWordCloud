b21fbd11106f7b0adff7d9f25d44fa321ae998f4
konbraphat51 <konbraphat@gmail.com>
2024-01-05 17:02:29 +0900

    fix: give raw data, not class

--- tests/Manual/AnimationVisualization.py
+++ tests/Manual/AnimationVisualization.py
@@ -3,13 +3,13 @@ Observes how animation words
 """
 
 from AnimatedWordCloud import Config, animate
-from tests.TestDataGetter import timelapses_test
+from tests.TestDataGetter import raw_timelapses_test
 
 # testing data
-timelapse = timelapses_test[0]
+raw_timelapse = raw_timelapses_test[0]
 
 config = Config(
     max_words=50, max_font_size=20, min_font_size=10, verbosity="debug"
 )
 
-animate(timelapse, config)
+animate(raw_timelapse, config)
