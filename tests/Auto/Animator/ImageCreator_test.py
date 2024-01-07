787be99e6467e46694b3dcf49b90da780145373e
konbraphat51 <konbraphat@gmail.com>
2024-01-08 01:07:58 +0900

    improve: assert create_images() produces more than 0

--- tests/Auto/Animator/ImageCreator_test.py
+++ tests/Auto/Animator/ImageCreator_test.py
@@ -26,7 +26,7 @@ def test_imagecreator():
     allocation_in_frame.words = {"word": (30, (50, 50))}  # dictionary
     position_in_frames.add("2023_04_01", allocation_in_frame)
     config = Config(intermediate_frames_id="test", verbosity="debug")
-    create_images(position_in_frames, config)
+    assert len(create_images(position_in_frames, config)) > 0
     test_path = os.path.join(DEFAULT_OUTPUT_PATH, "test_0.png")
     assert os.path.isfile(test_path)
     os.remove(os.path.join(DEFAULT_OUTPUT_PATH, "test_0.png"))
